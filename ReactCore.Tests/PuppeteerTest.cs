a7b57bd99bcb75c357654dc62ccb06a14bcc8f14
Michael Lawrence <mlawrence@gatesmandave.com>
2020-01-08 14:30:17 -0500

    added xunit test project

---
+++ ReactCore.Tests/PuppeteerTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using ReactCore.Controllers;
+using Moq;
+
+namespace ReactCore.Tests
+{
+    public class PuppeteerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
+/*
+https://github.com/moq/moq4
+https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/testing?view=aspnetcore-3.1
+https://stackoverflow.com/questions/970198/how-to-mock-the-request-on-controller-in-asp-net-mvc
+*/
